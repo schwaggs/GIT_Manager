b2a885f791dbab0c8a48860b154d36d39588c5ba
Josh <joshua.schweigert@mines.sdsmt.edu>
2017-12-17 23:37:17 -0700

    Added new git functionality

--- GITRepoManager/GITRepoManager/NewRepoMethods.cs
+++ GITRepoManager/GITRepoManager/NewRepoMethods.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace GITRepoManager
 {
@@ -10,7 +11,20 @@ namespace GITRepoManager
     {
         public static void CreateNewRepository()
         {
+            string cmd1 = "git init --bare " + NewRepoData.Repository_Setting_Location;
 
+            Process cmdProc = new Process();
+            cmdProc.StartInfo.FileName = "cmd.exe";
+            cmdProc.StartInfo.RedirectStandardInput = true;
+            cmdProc.StartInfo.RedirectStandardOutput = true;
+            cmdProc.StartInfo.CreateNoWindow = true;
+            cmdProc.StartInfo.UseShellExecute = false;
+            cmdProc.Start();
+
+            cmdProc.StandardInput.WriteLine(cmd1);
+            cmdProc.StandardInput.Flush();
+            cmdProc.StandardInput.Close();
+            cmdProc.Close();
         }
     }
 }
